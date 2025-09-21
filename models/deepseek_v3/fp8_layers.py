e3c515d25e13e468b83e51a22db77a1625850f6f
Matthew Noto <notomatthew31@gmail.com>
2025-09-21 02:30:37 -0700

    update

--- models/deepseek_v3/fp8_layers.py
+++ models/deepseek_v3/fp8_layers.py
@@ -9,29 +9,30 @@ import torch.nn.functional as F
 from typing import Optional, Tuple
 import deep_gemm
 from deep_gemm import ceil_div, get_mn_major_tma_aligned_tensor
+from deep_gemm.utils.math import per_token_cast_to_fp8, per_block_cast_to_fp8
 from models.deepseek_v3.fp8_layers_triton import per_token_cast_to_fp8_triton, per_block_cast_to_fp8_triton
 
 block_size = 128
 
-def per_token_cast_to_fp8(x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-    assert x.dim() == 2
-    m, n = x.shape
-    pad_size = (128 - (n % 128)) % 128
-    x = torch.nn.functional.pad(x, (0, pad_size), value=0) if pad_size > 0 else x
-    x_view = x.view(m, -1, 128)
-    x_amax = x_view.abs().float().amax(dim=2).view(m, -1).clamp(1e-4)
-    fp8_data = (x_view * (448.0 / x_amax.unsqueeze(2))).to(torch.float8_e4m3fn)
-    return fp8_data.view(m, n + pad_size)[:, :n], (x_amax / 448.0).view(m, -1)
+# def per_token_cast_to_fp8(x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+#     assert x.dim() == 2
+#     m, n = x.shape
+#     pad_size = (128 - (n % 128)) % 128
+#     x = torch.nn.functional.pad(x, (0, pad_size), value=0) if pad_size > 0 else x
+#     x_view = x.view(m, -1, 128)
+#     x_amax = x_view.abs().float().amax(dim=2).view(m, -1).clamp(1e-4)
+#     fp8_data = (x_view * (448.0 / x_amax.unsqueeze(2))).to(torch.float8_e4m3fn)
+#     return fp8_data.view(m, n + pad_size)[:, :n], (x_amax / 448.0).view(m, -1)
 
-def per_block_cast_to_fp8(x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-    assert x.dim() == 2
-    m, n = x.shape
-    x_padded = torch.zeros((ceil_div(m, 128) * 128, ceil_div(n, 128) * 128), dtype=x.dtype, device=x.device)
-    x_padded[:m, :n] = x
-    x_view = x_padded.view(-1, 128, x_padded.size(1) // 128, 128)
-    x_amax = x_view.abs().float().amax(dim=(1, 3), keepdim=True).clamp(1e-4)
-    x_scaled = (x_view * (448.0 / x_amax)).to(torch.float8_e4m3fn)
-    return x_scaled.view_as(x_padded)[:m, :n].contiguous(), (x_amax / 448.0).view(x_view.size(0), x_view.size(2))
+# def per_block_cast_to_fp8(x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+#     assert x.dim() == 2
+#     m, n = x.shape
+#     x_padded = torch.zeros((ceil_div(m, 128) * 128, ceil_div(n, 128) * 128), dtype=x.dtype, device=x.device)
+#     x_padded[:m, :n] = x
+#     x_view = x_padded.view(-1, 128, x_padded.size(1) // 128, 128)
+#     x_amax = x_view.abs().float().amax(dim=(1, 3), keepdim=True).clamp(1e-4)
+#     x_scaled = (x_view * (448.0 / x_amax)).to(torch.float8_e4m3fn)
+#     return x_scaled.view_as(x_padded)[:m, :n].contiguous(), (x_amax / 448.0).view(x_view.size(0), x_view.size(2))
 
 def functional_fp8_linear(x: torch.Tensor, weight, bias=None) -> torch.Tensor:
     """
@@ -65,13 +66,13 @@ class FP8Linear(torch.autograd.Function):
         # x_fp8 = per_token_cast_to_fp8_triton(x)
         x_fp8 = (x_fp8[0], get_mn_major_tma_aligned_tensor(x_fp8[1]))
 
-        weight_fp8 = per_block_cast_to_fp8(weight)
+        weight_fp8 = per_block_cast_to_fp8(weight, use_ue8m0=False)
         # weight_fp8 = per_block_cast_to_fp8_triton(weight)
         ctx.save_for_backward(x, weight)
         out_dim = weight.shape[0]
         # flattened
         out = torch.zeros((x.shape[0], out_dim), device=x.device, dtype=x.dtype)
-        deep_gemm.fp8_gemm_nt(x_fp8, weight_fp8, out)
+        deep_gemm.fp8_gemm_nt(x_fp8, weight_fp8, out, disable_ue8m0_cast=False)
         if len(shape) == 3:
             out = out.view(shape[0], shape[1], out_dim)
         return out
@@ -106,7 +107,7 @@ class FP8Linear(torch.autograd.Function):
         if ctx.needs_input_grad[0]:
             dy_fp8 = per_token_cast_to_fp8(grad_output.contiguous())
             dy_fp8 = (dy_fp8[0], get_mn_major_tma_aligned_tensor(dy_fp8[1]))
-            weight_fp8 = per_block_cast_to_fp8(weight.t().contiguous())
+            weight_fp8 = per_block_cast_to_fp8(weight.t().contiguous(), use_ue8m0=False)
             # weight_fp8 = (weight, weight.scale)
             # weight_fp8 = (weight_fp8[0].t().contiguous(), weight_fp8[1].t().contiguous())
 
